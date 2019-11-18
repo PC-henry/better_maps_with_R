ed448e5fda23e86954f80119d2bb93c71b8b425d
Singh <msingh@pc.gov.au>
2019-11-18 17:15:40 +1100

    Added comments for clarity

--- R/_Activity 1.R
+++ R/_Activity 1.R
@@ -25,14 +25,6 @@ head(shape)
 
 
 
-#Plot using ggplot2 and geom_sf()
-
-ggplot(shape) +
-  geom_sf()
-
-
-
-
 # Set SA2_MAIN16 variable to numeric
 
 shape <- shape %>% 
@@ -42,15 +34,26 @@ shape <- shape %>%
 
 
 
+#Plot using ggplot2 and geom_sf()
+
+ggplot(shape) +
+  geom_sf()
+
+
+# YOUR TURN
+# Re-produce the ggplot
+## Bonus content - Remove background information 
+# - remove background axis, labels and grid
+
 
 # Activity 1b. Make an interactive map
 
 leaflet(shape) %>% 
-  addTiles() %>% 
-  addPolygons(label = ~SA2_NAME16)
+  addTiles() %>% #adding layers to map. Using default base map - OpenStreetMap tiles.
+  addPolygons(label = ~SA2_NAME16) #adding SA2 polygons
 
 # YOUR TURN
-
+# Re-create SA2s in leaflet
 
 
 # Activity 1c. Differencing shapes 
@@ -58,27 +61,28 @@ leaflet(shape) %>%
 
 # Create a Lat/long of map boundaries
 
-aus_box <- rbind(
+aus_box <- rbind( #Combining vector by rows
   c(155, -9), 
   c(110, -9), 
   c(110, -45), 
   c(155, -45),
   c(155, -9)
 ) 
-
+summary(aus_box)
 
 
 # Plot boundary 
 
 leaflet(aus_box) %>% 
   addTiles() %>% 
-  addPolygons()
+  addPolygons() 
 
 
 
-# Save CRS
+# CRS is not defined here - see warning message
+# Save CRS from spatial file's metadata into shape_crs
 
-shape_crs <- st_crs(shape)
+shape_crs <- st_crs(shape) #retrieve the object's CRS
 
 
 
@@ -87,9 +91,9 @@ shape_crs <- st_crs(shape)
 
 aus_box <- aus_box %>% 
   list() %>%            # Convert to list
-  st_polygon() %>%      # Convert to polygon
-  st_sfc %>%            # Convert to column
-  st_sf %>%             # Convert to data frame
+  st_polygon() %>%      # Convert to sf polygon
+  st_sfc %>%            # Convert to sf column
+  st_sf %>%             # Convert to sf object 
   st_set_crs(shape_crs) # Set CRS
 
 
@@ -109,7 +113,9 @@ leaflet(shape) %>%
   addPolygons(label = ~SA2_NAME16)
 
 
-
+#YOUR TURN
+#Use your own custom lat/long coordinates to create boundaries
+#Or filter by State (STE_NAME16) to view specific States and Territories
 
 # Activity 1a. ggplot bonus content 
 # - removing background axis, labels and grid
